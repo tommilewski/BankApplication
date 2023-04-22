d5e70e2290baf8e7b30589264b7617e41017f895
Tomasz Milewski <tommilewski2003@gmail.com>
2023-04-22 10:42:16 +0200

    Added h2 database dependency in pom.xml and added application.properties in test pacakge and wrote first simple unit test for clientReposiotry

---
+++ src/test/java/com/example/BankApplication/repository/ClientRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.BankApplication.repository;
+
+import com.example.BankApplication.model.Client;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+
+@DataJpaTest
+public class ClientRepositoryTest {
+
+    @Autowired
+    private ClientRepository underTest;
+
+    @Test
+    public void itShouldCheckIfClientExistsEmail(){
+        //given
+        String email = "tommil@gmail.com";
+        Client client = new Client(1L, "Tomasz", email, List.of());
+        underTest.save(client);
+
+        //when
+        Client result = underTest.findByEmail(email);
+
+        //then
+        Assertions.assertEquals(client, result);
+
+    }
+
+}
