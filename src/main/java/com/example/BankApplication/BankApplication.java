50eb0d575b72b64056295c2506b84458916da8b4
Tomasz Milewski <tommilewski2003@gmail.com>
2023-04-29 12:17:23 +0200

    Trying use RestTemplete

--- src/main/java/com/example/BankApplication/BankApplication.java
+++ src/main/java/com/example/BankApplication/BankApplication.java
@@ -2,12 +2,27 @@ package com.example.BankApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class BankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankApplication.class, args);
+		//SpringApplication.run(BankApplication.class, args);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		String url = "https://api.exchangeratesapi.io/v1/latest";
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("access_key", "nYAlTG0eCVAS5W2NLqcjpjs8evZxOVJF");
+		uriVariables.put("base", "USD");
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class, uriVariables);
+		System.out.println(response.getBody());
 	}
 
 }
